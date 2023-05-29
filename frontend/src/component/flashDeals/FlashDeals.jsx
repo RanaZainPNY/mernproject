c09990f9f90569d4a85c84aac8afb708df515f8d
Kamran Dilshad <rana.kamran141947@gmail.com>
2023-05-29 11:37:22 +0500

    update card

--- frontend/src/component/flashDeals/FlashDeals.jsx
+++ frontend/src/component/flashDeals/FlashDeals.jsx
@@ -1,43 +1,24 @@
 import React from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
- import "./Card.css";
+import "./Card.css";
+import Product from "./Product";
+import { productData, responsive } from "./dataproduct";
 const FlashDeals = () => {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  };
+  const product = productData.map((item) => (
+    <Product
+      name={item.name}
+      url={item.imageurl}
+      price={item.price}
+      description={item.description}
+    />
+  ));
+
   return (
     <>
       <div className="mid">
         <h1>Flash Deals</h1>
-        <Carousel responsive={responsive}>
-          <div className="card">
-            <img src="./images/flash/flash-3.png" alt="" />
-            <h2> Antiques </h2>
-            <p className="price">$29</p>
-            <p>some description about produt</p>
-            <p> <button>Add to cart</button> </p>
-          </div>
-          <div>Item 2</div>
-          <div>Item 3</div>
-          <div>Item 4</div>
-        </Carousel>
+        <Carousel showDots={true} responsive={responsive}>{product}</Carousel>
       </div>
     </>
   );
