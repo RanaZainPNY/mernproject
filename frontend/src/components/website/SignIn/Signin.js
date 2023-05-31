4b867ec9c2eca899cac6a2eeb0b8e63d847d4725
farhanahmed0308 <farhanahmed2297@gmail.com>
2023-05-31 09:35:48 +0500

    login form

--- frontend/src/components/website/SignIn/Signin.js
+++ frontend/src/components/website/SignIn/Signin.js
@@ -3,73 +3,56 @@ import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 import "./Signin.css";
 
-const SigninSchema = Yup.object().shape({
-  email: Yup.string().email("Invalid email").required("Required"),
-  password: Yup.string()
-    .required("Password is required")
-    .min(8, "Password must be at least 8 characters long")
-    .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
-      "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
-    ),
-});
+
+// const SigninSchema = Yup.object().shape({
+//   email: Yup.string().email("Invalid email").required("Required"),
+//   password: Yup.string()
+//     .required("Password is required")
+//     .min(8, "Password must be at least 8 characters long")
+//     .matches(
+//       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+//       "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+//     ),
+// });
 
 const Signin = () => {
   return (
-    <div>
-      <div className="container-fluid  bg-dark vh-100 d-flex justify-content-center align-items-center">
-        <div className="container  w-50 bg-light ">
-          <div className="row">
-            <div className="container bg-success text-white mb-3 p-3">
-              <h1 className="text-center">Log In</h1>
-            </div>
-            <Formik
-              initialValues={{
-                email: "",
-                password: "",
-              }}
-              validationSchema={SigninSchema}
-              onSubmit={(values, actions) => {
-                console.log(values);
-                actions.resetForm();
-              }}
-            >
-              {({ errors, touched }) => (
-                <Form>
-                  <div className="form-group">
-                    <label htmlFor="email">
-                      <b>Email</b>
-                    </label>
-                    <Field name="email" type="email" className="form-control" />
-                    {errors.email && touched.email ? (
-                      <div className="red">{errors.email}</div>
-                    ) : null}
+    <>
+      <div className="main">
+        <div className="sub-main">
+          <div>
+                <div className="image">
+                  <div className="Container-image">
+                    <img src="profile3.jpeg" alt="its image" style={{borderRadius: "50%", maxWidth: "100px", maxHeight: "200px"}}/>
                   </div>
+                </div>
 
-                  <div className="form-group">
-                    <label htmlFor="password">
-                      <b>Password</b>
-                    </label>
-                    <Field
-                      name="password"
-                      type="password"
-                      className="form-control"
-                    />
-                    {errors.password && touched.password ? (
-                      <div className="red">{errors.password}</div>
-                    ) : null}
+                <div>
+                  <h1>Login page</h1>
+                  <div className="email-section">
+                    <img src="email.png" alt="email" className="email"/>
+                    <input  className="loginpage" type="text" placeholder="email"/>
                   </div>
+                  <div className="password-section">
+                    <img src="password.png" alt="password" className="password"/>
+                    <input className="loginpage" type="password" placeholder="password"/>
+                  </div>
+                  <div className="login-button">
+                  <button style={{borderRadius: "60px"}}>login</button>
+                  </div>
+                  <div>
+                    <p className="link">
+                      <a href="#">forget password?</a> or <a href="#">sign up</a>
+                    </p>
+                  </div>
+                </div>
 
-                  <button type="submit" className="btn btn-primary mb-2">
-                    Submit
-                  </button>
-                </Form>
-              )}
-            </Formik>
           </div>
         </div>
+        
+        
       </div>
-    </div>
+    </>
   );
 };
 
